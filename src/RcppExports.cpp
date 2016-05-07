39093bd95ba4aa9c8415d5fac66eef5c32514825
Devin Incerti <devin.incerti@gmail.com>
2016-05-06 19:47:27 -0700

    Second commit

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,19 +6,22 @@
 
 using namespace Rcpp;
 
-// markovCohort
-List markovCohort(arma::cube P, arma::rowvec z0, int ncycles, arma::mat state_costs, arma::mat state_qol, double discount);
-RcppExport SEXP cea_markovCohort(SEXP PSEXP, SEXP z0SEXP, SEXP ncyclesSEXP, SEXP state_costsSEXP, SEXP state_qolSEXP, SEXP discountSEXP) {
+// markovCohortC
+List markovCohortC(arma::cube P, arma::rowvec z0, int ncycles, arma::mat costs, arma::mat qol, double discount, arma::vec P_indx, arma::vec cost_indx, arma::vec qol_indx);
+RcppExport SEXP cea_markovCohortC(SEXP PSEXP, SEXP z0SEXP, SEXP ncyclesSEXP, SEXP costsSEXP, SEXP qolSEXP, SEXP discountSEXP, SEXP P_indxSEXP, SEXP cost_indxSEXP, SEXP qol_indxSEXP) {
 BEGIN_RCPP
     Rcpp::RObject __result;
     Rcpp::RNGScope __rngScope;
     Rcpp::traits::input_parameter< arma::cube >::type P(PSEXP);
     Rcpp::traits::input_parameter< arma::rowvec >::type z0(z0SEXP);
     Rcpp::traits::input_parameter< int >::type ncycles(ncyclesSEXP);
-    Rcpp::traits::input_parameter< arma::mat >::type state_costs(state_costsSEXP);
-    Rcpp::traits::input_parameter< arma::mat >::type state_qol(state_qolSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type costs(costsSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type qol(qolSEXP);
     Rcpp::traits::input_parameter< double >::type discount(discountSEXP);
-    __result = Rcpp::wrap(markovCohort(P, z0, ncycles, state_costs, state_qol, discount));
+    Rcpp::traits::input_parameter< arma::vec >::type P_indx(P_indxSEXP);
+    Rcpp::traits::input_parameter< arma::vec >::type cost_indx(cost_indxSEXP);
+    Rcpp::traits::input_parameter< arma::vec >::type qol_indx(qol_indxSEXP);
+    __result = Rcpp::wrap(markovCohortC(P, z0, ncycles, costs, qol, discount, P_indx, cost_indx, qol_indx));
     return __result;
 END_RCPP
 }
--- src/markov_cohort.cpp
+++ src/markov_cohort.cpp
@@ -4,56 +4,32 @@
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-List markovCohort(arma::cube P, arma::rowvec z0, int ncycles,
-                  arma::mat state_costs, arma::mat state_qol, double discount) {
+List markovCohortC(arma::cube P, arma::rowvec z0, int ncycles,
+                  arma::mat costs, arma::mat qol, double discount,
+                  arma::vec P_indx, arma::vec cost_indx, arma::vec qol_indx) {
   // Initialize
   double N = sum(z0);
   int n_states = P.n_cols;
-  int len_state_costs = state_costs.n_rows;
-  int len_state_qol = state_qol.n_rows;
-  int len_P = P.n_slices;
   arma::mat Z1(ncycles, n_states);
   arma::mat Z = join_cols(z0, Z1);
-  arma::vec qalys(ncycles + 1);
-  arma::vec costs(ncycles + 1);
+  arma::vec cycle_qalys(ncycles + 1);
+  arma::vec cycle_costs(ncycles + 1);
   double delta = 1;
 
   // Cycle 0
-  qalys(0) = 0;
-  costs(0) = 0;
+  cycle_qalys(0) = 0;
+  cycle_costs(0) = 0;
 
-  // Markov Chain
+  //Cycles 1 - T
   for (int t = 1; t <= ncycles; ++t){
     delta = pow(1/(1 + discount), t);
-
-    // state vector
-    if(len_P == 1){
-      Z.row(t) = Z.row(t-1) * P.slice(0);
-    }
-    else{
-      Z.row(t) = Z.row(t-1) * P.slice(t-1);
-    }
-
-    // costs
-    if (len_state_costs == 1){
-      costs(t) = delta * dot(Z.row(t), state_costs.row(0))/N;
-    }
-    else {
-      costs(t) = delta * dot(Z.row(t), state_costs.row(t))/N;
-    }
-
-    // qalys
-    if (len_state_qol == 1){
-      qalys(t) = delta * dot(Z.row(t), state_qol.row(0))/N;
-    }
-    else {
-      qalys(t) = delta * dot(Z.row(t), state_qol.row(t))/N;
-    }
+    Z.row(t) = Z.row(t - 1) * P.slice(P_indx(t - 1) - 1);
+    cycle_costs(t) = delta * dot(Z.row(t), costs.row(cost_indx(t - 1) - 1))/N;
+    cycle_qalys(t) = delta * dot(Z.row(t), qol.row(qol_indx(t - 1) - 1))/N;
   }
 
   return List::create(Named("state") = Z,
-                      Named("costs") = costs,
-                      Named("qalys") = qalys,
-                      Named("delta") = delta);
+                      Named("costs") = cycle_costs,
+                      Named("qalys") = cycle_qalys);
 }
 
