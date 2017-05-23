d18303977de864fdc204ed21da070d1b6805866b
Devin.Incerti <devin.incerti@OAK-MB-DINCERTI.local>
2017-05-22 20:28:38 -0700

    Rename sim_ctsm.R

--- data-raw/lifetable.R
+++
@@ -1,7 +0,0 @@
-lifetable <- read.csv("data-raw/lifetable.csv")
-lifetable_male <- lifetable[lifetable$male == 1, ]
-lifetable_female <- lifetable[lifetable$male == 0, ]
-lifetable_male$male <- NULL
-lifetable_female$male <- NULL
-save(lifetable_male, file = "data/lifetable-male.rda", compress = "bzip2")
-save(lifetable_female, file = "data/lifetable-female.rda", compress = "bzip2")
