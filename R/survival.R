f794da222d9139c0fccbcf2665b2ff4a380ec7ce
Incerti <devin.incerti@gmail.com>
2017-12-11 22:24:41 -0800

    Update rsample so that flexsurvreg and flexsurvspline are the same object; also add funtionality to sample from lists of flexsurvreg objects

--- R/survival.R
+++ R/survival.R
@@ -8,41 +8,43 @@
 #' @param ... Additional arguments to pass to random sampling functions.
 #' @return The form of \code{rsample} depends on the class of its argument. See the 
 #' documentation of the particular methods for details of what is produced.
-#' @seealso \link{rsample.flexsurvreg}, \link{rsample.flexsurvspline}
+#' @seealso \link{rsample.flexsurvreg}, \link{rsample.list}
 #' @name rsample
 #' @export
 rsample <- function (x, n, ...) {
   UseMethod("rsample", x)
 }
 
-rsample_flexsurv <- function(x, n){
+rsample_flexsurv1 <- function(x, n){
   sim <- flexsurv::normboot.flexsurvreg(x, B = n, raw = TRUE)
   n.pars <- length(x$dlist$pars)
-  par.samples <- vector(length = n.pars, mode = "list")
-  names(par.samples) <- x$dlist$pars
+  par.samples <- vector(length = n.pars + 1, mode = "list")
+  names(par.samples) <- c(x$dlist$pars, "dist")
   for (j in seq_along(x$dlist$pars)){
     parname.j <-  x$dlist$pars[j]
     covind.j <- x$mx[[parname.j]]
     if (length(covind.j) > 0){
-      ind.j <- c(j, n.pars + covind.j)
+        ind.j <- c(j, n.pars + covind.j)
     } else{
-      ind.j <- j
+        ind.j <- j
     }
     par.samples[[j]] <- sim[, ind.j, drop = FALSE]
   }
+  par.samples[["dist"]] <- x$dlist$name
   return(par.samples)
 }
 
-#' Randomly sample parameters from a parametric survival model
+#' Randomly sample parameters from a flexsurvreg model
 #'
 #' Draw random samples from a survival model fit using 
-#' \code{flexsurv::flexsurvreg}.
+#' \code{flexsurv::flexsurvreg} or \code{flexsurv::flexsurvspline}.
 #'
 #' @param n Number of random observations to draw.
-#' @param x Output from \link{flexsurvreg} from the package \link{flexsurv}.
+#' @param x Output from \link{flexsurvreg} from the package \link{flexsurv}. 
 #' @param ... Further arguments passed to or from other methods. Currently unused.
-#' @return List where each element contains random samples of the coefficients
-#' used to predict a given parameter of the survival distribution. 
+#' @return Returns an object of \link{class} "surv_pars", which is a list 
+#' containing (i) random samples of the regression coefficients used to predict
+#' each of the model parameters and (ii) the probability distribution. 
 #' @name rsample.flexsurvreg
 #' @examples 
 #' library("flexsurv")
@@ -53,29 +55,44 @@ rsample_flexsurv <- function(x, n){
 #' head(fit.sample)
 #' @export
 rsample.flexsurvreg <- function(x, n, ...){
-  return(rsample_flexsurv(x, n))
+  sim <- rsample_flexsurv1(x, n)
+  class(sim) <- "surv_pars"
+  return(sim)
 }
 
-#' Randomly sample parameters from a spline-based survival model
+#' Randomly sample parameters from a list of flexsurvreg models
 #'
-#' Draw random samples from a survival model fit using 
-#' \code{flexsurv::flexsurvspline}.
+#' Draw random samples from a list of survival models fit using 
+#' \code{flexsurv::flexsurvreg} or \code{flexsurv::flexsurvspline}.
 #'
 #' @param n Number of random observations to draw.
-#' @param x Output from \link{flexsurvspline} from the package \link{flexsurv}.
+#' @param x A list of models fit using \link{flexsurvreg} from the package
+#'  \link{flexsurv}. 
 #' @param ... Further arguments passed to or from other methods. Currently unused.
-#' @return List where each element contains random samples of the coefficients
-#' used to predict a given parameter of the survival distribution. 
-#' @name rsample.flexsurvspline
+#' @return Returns an object of \link{class} "survlist_pars", which is a list 
+#' of lists. Each list contains (i) random samples of the regression coefficients 
+#' used to predict each of the model parameters and (ii) the
+#'  probability distribution.
+#' @name rsample.list
 #' @examples 
 #' library("flexsurv")
-#' fit <- flexsurv::flexsurvspline(Surv(recyrs, censrec) ~ group, data = bc, k=1, 
-#'                      scale = "hazard")
-#' fit.sample <- rsample(fit, n = 2)
-#' head(fit.sample)
+#' fit1 <- flexsurv::flexsurvreg(formula = Surv(futime, fustat) ~ age, 
+#'                     data = ovarian, dist = "exponential")
+#' fit2 <- flexsurv::flexsurvreg(formula = Surv(futime, fustat) ~ 1, 
+#'                    data = ovarian, dist = "gamma")
+#' fits <- list(f1 = fit1, f2 = fit2)
+#' fits.sample <- rsample(fits, n = 2)
+#' head(fits.sample)
 #' @export
-rsample.flexsurvspline <- function(x, n, ...){
-  return(rsample_flexsurv(x, n))
+rsample.list <- function(x, n, ...){
+  ret <- vector(length(x), mode = "list")
+  for (i in 1:length(x)){
+    if (!inherits(x[[i]], "flexsurvreg")){
+      stop("Each element of x must be of class flexsurvreg")
+    }
+    ret[[i]] <- rsample_flexsurv1(x[[i]], n)
+  } 
+  class(ret) <- "survlist_pars"
+  return(ret)
 }
-
 
--- tests/testthat/test-survival.R
+++ tests/testthat/test-survival.R
@@ -4,48 +4,58 @@ library("flexsurv")
 # Sample parameters ------------------------------------------------------------
 test_that("rsample.flexsurvreg", {
   
-  # covariates on no paramters
+  # error when not an object of class flexsurvreg 
+  expect_error(rsample(list(x = 5), 2))
+  expect_error(rsample(matrix(1), n = 2))
+  
+  # covariates on no parameters
   fit <- flexsurvreg(formula = Surv(futime, fustat) ~ 1, 
                      data = ovarian, dist = "weibull")
   n <- 5
   set.seed(101)
-  fit.sample <- rsample.flexsurvreg(fit, n = n)
+  fit.sample <- rsample(fit, n = n)
   set.seed(101)
   fit.normboot <- normboot.flexsurvreg(fit, B = n)
-  expect_equal(length(fit.sample), length(fit$dlist$pars))
+  expect_equal(length(fit.sample), length(fit$dlist$pars) + 1)
   expect_equal(c(fit.sample$shape), fit.normboot[, "shape"])
   expect_equal(nrow(fit.sample$scale), n)
-  expect_false(any(lapply(fit.sample, ncol) != 1))
+  expect_false(any(lapply(fit.sample[1:2], ncol) != 1))
+  expect_true(inherits(fit.sample, "surv_pars"))
   
   ## sample of size 1
-  expect_error(rsample.flexsurvreg(fit, n = 1), NA)
-  
-  ## check rsample()
-  expect_error(rsample(fit, n = n), NA)
+  expect_error(rsample(fit, n = 1), NA)
   
   # covariates on 1 paramters
   ## lognormal
   fit <- flexsurvreg(formula = Surv(futime, fustat) ~ age, 
                        data = ovarian, dist = "lognormal")
-  fit.sample <- rsample.flexsurvreg(fit, n = 3)
+  fit.sample <- rsample(fit, n = 3)
   expect_equal(ncol(fit.sample$meanlog), 2)
   expect_equal(ncol(fit.sample$sdlog), 1)
   
   # covariates on 2 parameters
   fit <- flexsurvreg(Surv(recyrs, censrec) ~ group, data = bc,
                        anc = list(sigma = ~ group), dist = "gengamma") 
-  fit.sample <- rsample.flexsurvreg(fit, n = 2)
+  fit.sample <- rsample(fit, n = 2)
   expect_equal(ncol(fit.sample$mu), 3)
   expect_equal(ncol(fit.sample$sigma), 3)
   expect_equal(ncol(fit.sample$Q), 1)
-})
-
-test_that("sample.flexsurvspline", {
+  
+  # spline
   fit <- flexsurvspline(Surv(recyrs, censrec) ~ group, data = bc, k=1, 
                         scale = "hazard")
-  fit.normboot <- rsample(fit, n = 2)
-  expect_error(fit.normboot, NA)
-
-
+  fit.sample <- rsample(fit, n = 2)
+  expect_error(fit.sample, NA)
+  
+  # multiple models
+  fit1 <- flexsurvreg(formula = Surv(futime, fustat) ~ age, 
+                      data = ovarian, dist = "exponential")
+  fit2 <- flexsurvreg(formula = Surv(futime, fustat) ~ 1, 
+                        data = ovarian, dist = "gamma")
+  fits <- list(f1 = fit1, f2 = fit2)
+  fits.sample <- rsample(fits, n = 2)
+  expect_equal(fits.sample[[1]]$dist, "exp")
+  expect_equal(fits.sample[[2]]$dist, "gamma")
+  expect_true(inherits(fits.sample, "survlist_pars"))
+  expect_error(rsample(list(x = 1), n = 1))
 })
-
