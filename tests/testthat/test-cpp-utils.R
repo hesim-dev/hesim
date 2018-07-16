d568558f8d43d874922d5183c5e309332dae5ce4
Incerti <devin.incerti@gmail.com>
2018-07-16 16:28:23 -0700

    Fix error in tests from last commit

--- tests/testthat/test-cpp-utils.R
+++ tests/testthat/test-cpp-utils.R
@@ -5,7 +5,7 @@ test_that("add_constant", {
   v1 <- c(1, 2, 3)
   v2 <- c(1.2, 4, 5.6)
   expect_equal(hesim:::C_test_add_constant_int(v1, 5.5),
-               floor(v + 5.5))
+               floor(v1 + 5.5))
   expect_equal(hesim:::C_test_add_constant_double(v2, 5.5),
                v2 + 5.5)
 })
