199754d7f92a0e8ebfae6c775045181cca279d8e
Incerti <devin.incerti@gmail.com>
2018-08-03 07:46:10 -0700

    Enforce snake_case for bootstrap.R

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -18,34 +18,34 @@ bootstrap <- function (object, B, ...) {
 #' @name bootstrap
 #' @export
 bootstrap.partsurvfit <- function(object, B, max_errors = 0, ...){
-  n.obs <- nrow(object$data)
-  n.models <- length(object$models)
-  boot <- vector(mode = "list", length = n.models)
-  for (j in 1:n.models){
+  n_obs <- nrow(object$data)
+  n_models <- length(object$models)
+  boot <- vector(mode = "list", length = n_models)
+  for (j in 1:n_models){
     boot[[j]] <- matrix(NA, nrow = B, ncol = nrow(object$models[[j]]$res.t))
     colnames(boot[[j]]) <- rownames(object$models[[j]]$res.t)
   }
   
   # Bootstrap replications
-  n.errors <- 0
+  n_errors <- 0
   i <- 1
   while(i <= B){
-    index <- sample(x = 1:n.obs, size = n.obs, replace = TRUE)
-    data.i <- object$data[index, ]
-    fits <- vector(mode = "list", length = n.models)
-    error.j <- 0
-    for (j in 1:n.models){
+    index <- sample(x = 1:n_obs, size = n_obs, replace = TRUE)
+    data_i <- object$data[index, ]
+    fits <- vector(mode = "list", length = n_models)
+    error_j <- 0
+    for (j in 1:n_models){
       object$models[[j]]$call$formula <- object$models[[j]]$all.formulae[[1]]
       fit <- try(stats::update(object$models[[j]],
                     #formula = object$models[[j]]$all.formulae[[1]], 
                     # anc = object$models[[j]]$all.formulae[-1], but don't think should be needed
-                    data = data.i),
+                    data = data_i),
                  silent = FALSE)
       if(inherits(fit ,"try-error")){
-        n.errors <- n.errors + 1
-        error.j <- error.j + 1
-        if (n.errors > max_errors){
-              msg <- paste0("There were ", n.errors, " errors when fitting the statistical model, ",
+        n_errors <- n_errors + 1
+        error_j <- error_j + 1
+        if (n_errors > max_errors){
+              msg <- paste0("There were ", n_errors, " errors when fitting the statistical model, ",
                   "which is greater than the maximum number of allowed errors (max_errors = ",
                   max_errors, ").")
               stop(msg)
@@ -55,33 +55,33 @@ bootstrap.partsurvfit <- function(object, B, max_errors = 0, ...){
         boot[[j]][i, ] <- fit$res.t[, "est"]
       }
     }
-    if (error.j == 0){
+    if (error_j == 0){
       i <- i + 1
     }
   } # end bootstrap loop
-  if (n.errors > 0){
-    msg <- paste0("There were ", n.errors, " errors when fitting the statistical model ",
+  if (n_errors > 0){
+    msg <- paste0("There were ", n_errors, " errors when fitting the statistical model ",
                   "during the bootstrap procedure.")
     warning(msg)
   }
   
   # Output for params_surv_list
-  params.surv.list <- vector(mode = "list", length = n.models)
-  coefs <- vector(mode = "list", length = n.models)
-  for (j in 1:n.models){
-    n.pars <- length(object$models[[j]]$dlist$pars)
-    inds.j <- flexsurvreg_inds(object$models[[j]])
-    coefs[[j]] <- vector(length = n.pars, mode = "list")
+  params_surv_list <- vector(mode = "list", length = n_models)
+  coefs <- vector(mode = "list", length = n_models)
+  for (j in 1:n_models){
+    n_pars <- length(object$models[[j]]$dlist$pars)
+    inds_j <- flexsurvreg_inds(object$models[[j]])
+    coefs[[j]] <- vector(length = n_pars, mode = "list")
     names(coefs[[j]]) <- c(object$models[[j]]$dlist$pars)
-    for (k in 1:n.pars){
-      coefs[[j]][[k]] <- boot[[j]][, inds.j[[k]], drop = FALSE]
+    for (k in 1:n_pars){
+      coefs[[j]][[k]] <- boot[[j]][, inds_j[[k]], drop = FALSE]
     }
-    params.surv.list[[j]] <- new_params_surv(coefs = coefs[[j]],
+    params_surv_list[[j]] <- new_params_surv(coefs = coefs[[j]],
                                              dist = object$models[[j]]$dlist$name,
                                              n_samples = B,
                                              aux = flexsurvreg_aux(object$models[[j]]))
   }
-  names(params.surv.list) <- names(object$models)
-  class(params.surv.list) <- "params_surv_list"
-  return(params.surv.list)
+  names(params_surv_list) <- names(object$models)
+  class(params_surv_list) <- "params_surv_list"
+  return(params_surv_list)
 }
