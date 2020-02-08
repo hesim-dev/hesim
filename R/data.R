57c4adcfc3bc0fb2d922209ba7d3597bae001958
dincerti <devin.incerti@gmail.com>
2020-02-08 15:58:17 -0800

    Add multinomial example data

--- R/data.R
+++ R/data.R
@@ -48,12 +48,11 @@
 #' A collection of example datasets containing health state transition, 
 #' costs, and utility data for a 3-state continuous time state transition model.
 #'
-#' 
 #' @format 
 #' A list containing the following elements:
 #' \itemize{
 #' \item{transitions}{ A data frame containing the times at which patient transitions between health states based
-#' on the dataset \link[mstate]{prothr} from the \code{mstate} package.}
+#' on the [prothr][mstate::prothr] dataset from the [mstate][mstate::mstate] package.}
 #' \item{costs}{ A list of data frames. The first data frame contains summary medical cost estimates and the
 #' second data frame contains drug cost data.}
 #' \item{utility}{ A data frame of summary utility estimates.}
@@ -76,7 +75,14 @@
 #' }
 #' 
 #' @section Cost data:
-#' The cost list contains two data frames. The first data frame contains summary data on
+#' The cost list contains two data frames. The first data frame contains 
+#' data on the drug costs associated with each treatment strategy.
+#' \describe{
+#'  \item{strategy_id}{The treatment strategy identification number.}
+#'  \item{costs}{Annualized drug costs.}
+#'  }
+#' 
+#' The second data frame contains summary data on
 #' medical costs by health state, and contains the following columns:
 #' \describe{
 #'   \item{state_id}{The health state identification number.}
@@ -84,19 +90,73 @@
 #'   \item{se}{Standard error of medical costs.}
 #'  }
 #'  
-#' The second data frame contains data on the drug costs associated with each treatment strategy.
+#' @section Utility data:
+#' The data frame has the following columns:
+#' \describe{
+#' \item{state_id}{The health state identification number.}
+#' \item{mean}{Mean utility}
+#' \item{se}{Standard error of utility}
+#' }
+#' 
+"ctstm3_exdata"
+
+#' Example data for multinomial model
+#'
+#' Example discrete time health state transitions data simulated using 
+#' multinomial logistic regression. Costs and utility 
+#' are also included to facilitate cost-effectiveness analysis. 
+#'
+#' @format 
+#' A list containing the following elements:
+#' \itemize{
+#' \item{transitions}{ A data frame containing patient transitions between health
+#' states at discrete time intervals (i.e., on a yearly basis).}
+#' \item{costs}{ A list of data frames. The first data frame contains 
+#' drug cost data and the second contains summary medical cost estimates.}
+#' \item{utility}{ A data frame of summary utility estimates.}
+#' }
+#' 
+#' @section Transitions data:
+#' The data frame has the following columns:
+#' \describe{
+#' \item{patient_id}{Patient identification number.}
+#' \item{strategy_id}{Treatment strategy identification number.}
+#' \item{strategy_name}{Treatment strategy name.}
+#' \item{age}{Patient age (in years).}
+#' \item{age_cat}{A factor variable with 3 age groups: (i) age less than 40, 
+#' (ii) age at least 40 and less than 60, and (iii) age at least 60.}
+#' \item{female}{1 if a patient is female; 0 if male.}
+#' \item{year}{The year since the start of data collection with the first year equal to 1.}
+#' \item{from}{Starting state.}
+#' \item{to}{Receiving state.}
+#' \item{year_cat}{Factor variable for year with 3 categories: (i) year 3 and below, (ii)
+#' year between 3 and 6, and (iii) year 7 and above.}
+#' }
+#' 
+#' @section Cost data:
+#' The cost list contains two data frames. The first data frame contains 
+#' data on the drug costs associated with each treatment strategy.
 #' \describe{
 #'  \item{strategy_id}{The treatment strategy identification number.}
-#'  \item{costs}{Annualized drug costs.}
+#'   \item{strategy_name}{The treatment strategy name.}
+#'   \item{costs}{Annualized drug costs.}
+#'  }
+#' 
+#' The second data frame contains summary data on
+#' medical costs by health state, and contains the following columns:
+#' \describe{
+#'   \item{state_id}{The health state identification number.}
+#'   \item{state_name}{The name of the health state.}
+#'   \item{mean}{Mean medical costs.}
+#'   \item{se}{Standard error of medical costs.}
 #'  }
 #'  
 #' @section Utility data:
 #' The data frame has the following columns:
 #' \describe{
 #' \item{state_id}{The health state identification number.}
-#' \item{mean}{Mean costs.}
-#' \item{se}{Standard error.}
+#' \item{state_name}{The name of the health state.}
+#' \item{mean}{Mean utility}
+#' \item{se}{Standard error of utility.}
 #' }
-#' 
-"ctstm3_exdata"
-
+"multinom3_exdata"
