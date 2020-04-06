3330d10f6f6c556a416fd39d37c621c91f1eee4a
dincerti <devin.incerti@gmail.com>
2020-04-06 07:42:04 -0700

    rdirichlet_mat works if the number of rows is greater than the number of columns if output = 'array'

--- R/rand.R
+++ R/rand.R
@@ -99,7 +99,8 @@ rpwexp <- function(n, rate = 1, time = 0){
 #'
 #' @param n Number of samples to draw.
 #' @param alpha A matrix where each row is a separate vector of shape parameters.
-#' @param output Whether output should be an array or a matrix. 
+#' @param output The class of the object returned by the function. Either an 
+#' `array`, `matrix`, `data.frame`, or `data.table`.
 #' @name rdirichlet_mat
 #' @examples
 #' alpha <- matrix(c(100, 200, 500, 50, 70, 75), ncol = 3, nrow = 2, byrow = TRUE)
@@ -107,16 +108,15 @@ rpwexp <- function(n, rate = 1, time = 0){
 #' print(samp[, , 1:2])
 #' @details This function is meant for representing the distribution of 
 #' transition probabilities in a transition matrix. The `(i,j)` element of
-#' `alpha` is a transition from state `i` to state `j`. The number of rows in
-#' `alpha` must be less than or equal to the number of columns.
-#'  \code{rdirichlet_mat} is vectorized and written in C++ for speed. 
+#' `alpha` is a transition from state `i` to state `j`. It is vectorized and 
+#' written in `C++` for speed. 
 #' @return If `output = "array"`, then an array of matrices is returned 
 #' where each row of each matrix is a sample from the Dirichlet distribution.
-#' If `output = "matrix"`, then a matrix is returned where each row contains
-#' all elements of the sampled matrix from the Dirichlet distribution ordered rowwise; 
-#' when `output = "data.frame"` or `output = "data.table"` the returned
-#' value is a `data.frame` or `data.table` in the same format as the
-#' matrix returned with `output = "matrix"`. 
+#' If `output` results in a two dimensional object (i.e., a `matrix`, 
+#' `data.frame`, or `data.table`, then each row contains
+#' all elements of the sampled matrix from the Dirichlet distribution 
+#' ordered rowwise; that is, each matrix is flattened. In these cases, 
+#' the number of rows must be less than or equal to the number of columns.  
 #' @export
 rdirichlet_mat <- function(n, alpha, output = c("array", "matrix", "data.frame", 
                                                 "data.table")){
@@ -130,12 +130,12 @@ rdirichlet_mat <- function(n, alpha, output = c("array", "matrix", "data.frame",
   if (!is.matrix(alpha)){
     alpha <- matrix(alpha, nrow = 1)
   }
-  if (nrow(alpha) > ncol(alpha)){
-    stop(paste0("The number of rows of 'alpha' must be less than or equal to ",
-                "the number of columns."))
-  }
   samp <- C_rdirichlet_mat(n, alpha)
   if (output %in% c("matrix", "data.frame", "data.table")){
+    if (nrow(alpha) > ncol(alpha)){
+      stop(paste0("The number of rows of 'alpha' must be less than or equal to ",
+                  "the number of columns unless 'output = arrary'."))
+    }
     samp <- matrix(c(aperm(samp, perm = c(2, 1, 3))),
                    ncol = dim(samp)[1] * dim(samp)[2], byrow = TRUE)
     colnames(samp) <- paste0("prob_", 1:ncol(samp))
