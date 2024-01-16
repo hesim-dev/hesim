93ae7706d3864082731632b536986db1da24bd92
dincerti <devin.incerti@gmail.com>
2024-01-15 16:54:53 -0800

    docs: add new special sentinel _PACKAGE

--- R/hesim.R
+++ R/hesim.R
@@ -6,16 +6,13 @@ data.table::as.data.table
 #' @export
 ggplot2::autoplot
 
-#' `hesim`: Health Economic Simulation Modeling and Decision Analysis
-#' 
 #' @description 
 #' To learn more about `hesim` visit the [website](https://hesim-dev.github.io/hesim/).
 #'
-#' @docType package
 #' @name hesim
 #' @useDynLib hesim
 #' @import data.table
 #' @importFrom Rcpp evalCpp
 #' @importFrom ggplot2 .data
 #' @keywords internal
-NULL
+"_PACKAGE"
